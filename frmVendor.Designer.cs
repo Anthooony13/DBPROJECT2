9728b9f6ef0daee116f475b4a839341062193cd4
Anthooony1 <quianzon.edward@gmail.com>
2024-05-14 22:25:30 +0800

    Vendor

---
+++ frmVendor.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace DBPROJECT
+{
+    partial class frmVendor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVendor));
+            this.bNavMain = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.txtSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.btnSearch = new System.Windows.Forms.ToolStripButton();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.bNavMain)).BeginInit();
+            this.bNavMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bNavMain
+            // 
+            this.bNavMain.AddNewItem = null;
+            this.bNavMain.CountItem = this.bindingNavigatorCountItem;
+            this.bNavMain.DeleteItem = null;
+            this.bNavMain.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.bNavMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.txtSearch,
+            this.btnSearch});
+            this.bNavMain.Location = new System.Drawing.Point(0, 0);
+            this.bNavMain.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bNavMain.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bNavMain.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bNavMain.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bNavMain.Name = "bNavMain";
+            this.bNavMain.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.bNavMain.PositionItem = this.bindingNavigatorPositionItem;
+            this.bNavMain.Size = new System.Drawing.Size(800, 31);
+            this.bNavMain.TabIndex = 5;
+            this.bNavMain.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 28);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(28, 28);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(28, 28);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 31);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(28, 28);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(28, 28);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 31);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(150, 31);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
+            this.btnSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(28, 28);
+            this.btnSearch.Text = "toolStripButton1";
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Location = new System.Drawing.Point(121, 74);
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowHeadersWidth = 62;
+            this.dgvMain.Size = new System.Drawing.Size(555, 360);
+            this.dgvMain.TabIndex = 4;
+            this.dgvMain.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvVen_CellValueChanged);
+            this.dgvMain.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dgvVen_RowPostPaint);
+            this.dgvMain.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dgvVen_RowPrePaint);
+            this.dgvMain.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dgvVen_UserDeletingRow);
+            this.dgvMain.DoubleClick += new System.EventHandler(this.dgvVen_DoubleClick);
+            // 
+            // frmVendor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bNavMain);
+            this.Controls.Add(this.dgvMain);
+            this.Name = "frmVendor";
+            this.Text = "frmVendor";
+            ((System.ComponentModel.ISupportInitialize)(this.bNavMain)).EndInit();
+            this.bNavMain.ResumeLayout(false);
+            this.bNavMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingNavigator bNavMain;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripTextBox txtSearch;
+        private System.Windows.Forms.ToolStripButton btnSearch;
+        private System.Windows.Forms.DataGridView dgvMain;
+    }
+}
---
+++ frmVendor.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBPROJECT
+{
+    public partial class frmVendor : Form
+    {
+
+        DataTable DTable;
+
+        SqlDataAdapter DAdapter;
+        SqlCommand DCommand;
+        BindingSource DBindingSource;
+
+        Boolean CancelUpdates;
+
+        int idcolumn = 0;
+        private object dgvVen;
+        private object bNavVen;
+        private int vendorid;
+
+        public frmVendor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVendors_Load(object sender, EventArgs e)
+        {
+            this.CancelUpdates = true;
+            this.BindMainGrid();
+            this.FormatGrid();
+            this.CancelUpdates = false;
+        }
+
+        private void BindMainGrid()
+        {
+            this.CancelUpdates = true;
+            if (Globals.glOpenSqlConn())
+            {
+                this.DCommand = new SqlCommand("spGetAllVendors", Globals.sqlconn);
+                this.DAdapter = new SqlDataAdapter(this.DCommand);
+
+                this.DTable = new DataTable();
+
+                this.DAdapter.Fill(DTable);
+
+                this.DBindingSource = new BindingSource();
+                this.DBindingSource.DataSource = DTable;
+
+                dgvVen.DataSource = DBindingSource;
+                this.bNavVen.BindingSource = this.DBindingSource;
+            }
+            this.CancelUpdates = false;
+        }
+        private void FormatGrid()
+        {
+            this.dgvVen.Columns["idVendor"].Visible = false;
+            this.dgvVen.Columns["nameVendor"].HeaderText = "Login Name";
+            this.dgvVen.Columns["addressVendor"].HeaderText = "Address";
+            this.dgvVen.Columns["emailVendor"].HeaderText = "Email";
+            this.dgvVen.Columns["contactVendor"].HeaderText = "Contact";
+
+            this.BackColor = Globals.gDialogBackgroundColor;
+
+            this.dgvVen.BackgroundColor = Globals.gGridOddRowColor;
+            this.dgvVen.AlternatingRowsDefaultCellStyle.BackColor = Globals.gGridEvenRowColor;
+
+            this.dgvVen.EnableHeadersVisualStyles = false;
+            this.dgvVen.ColumnHeadersDefaultCellStyle.BackColor = Globals.gGridHeaderColor;
+        }
+
+        private void dgvVen_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            using (SolidBrush b = new SolidBrush(((DataGridView)sender).RowHeadersDefaultCellStyle.ForeColor))
+
+            {
+
+                e.Graphics.DrawString(
+
+                    String.Format("{0,10}", (e.RowIndex + 1).ToString()),
+
+                    e.InheritedRowStyle.Font, b, e.RowBounds.Location.X + 10, e.RowBounds.Location.Y + 4);
+
+            }
+        }
+
+        private void dgvVen_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            int firstDisplayedCellIndex = dgvVen.FirstDisplayedCell.RowIndex;
+
+            int lastDisplayedCellIndex = firstDisplayedCellIndex + dgvVen.DisplayedRowCount(true);
+
+
+            Graphics Graphics = dgvVen.CreateGraphics();
+
+            int measureFirstDisplayed = (int)(Graphics.MeasureString(firstDisplayedCellIndex.ToString(), dgvVen.Font).Width);
+
+            int measureLastDisplayed = (int)(Graphics.MeasureString(lastDisplayedCellIndex.ToString(), dgvVen.Font).Width);
+
+
+            int rowHeaderWitdh = System.Math.Max(measureFirstDisplayed, measureLastDisplayed);
+
+            dgvVen.RowHeadersWidth = rowHeaderWitdh + 40;
+
+        }
+
+        private void dgvVen_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+
+                long venomerid = 0;
+                long newvenomerid;
+
+                if (this.CancelUpdates == false && this.dgvVen.CurrentRow != null)
+
+                {
+
+                    if (Globals.glOpenSqlConn())
+
+                    {
+
+                        DataGridViewRow row = dgvVen.CurrentRow;
+
+                        String venname = row.Cells["nameVendor"].Value == DBNull.Value ? ""
+
+                        : row.Cells["nameVendor"].Value.ToString().ToUpper();
+
+                        String venadd = row.Cells["addressVendor"].Value == DBNull.Value ? ""
+
+                        : row.Cells["addressVendor"].Value.ToString();
+
+                        String venemail = row.Cells["emailVendor"].Value == DBNull.Value ? ""
+
+                        : row.Cells["emailVendor"].Value.ToString();
+
+                        String vennum = row.Cells["contactVendor"].Value == DBNull.Value ? ""
+
+                        : row.Cells["contactVendor"].Value.ToString();
+
+
+
+
+                        if (row.Cells["nameVendor"].Value == DBNull.Value)
+
+                        {
+
+                            csMessageBox.Show("Please encode a valid user name", "Warning",
+
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                            object value = dgvVen.CancelEdit();
+
+                        }
+
+                        else
+
+                        {
+
+                            try
+
+                            {
+
+                                SqlCommand cmd = new SqlCommand("spVendorAddEdit", Globals.sqlconn);
+
+                                cmd.CommandType = CommandType.StoredProcedure;
+
+                                if (row.Cells[this.idcolumn].Value == DBNull.Value)
+
+                                    vendorid = 0;
+
+                                else
+
+                                    vendorid = Convert.ToInt64(row.Cells[this.idcolumn].Value);
+
+                                cmd.Parameters.AddWithValue("@vid", vendorid);
+
+                                cmd.Parameters.AddWithValue("@vname", venname);
+
+                                cmd.Parameters.AddWithValue("@vaddress", venadd);
+
+                                cmd.Parameters.AddWithValue("@vemail", venemail);
+
+                                cmd.Parameters.AddWithValue("@vnum", vennum);
+
+
+
+                                SqlDataAdapter dAdapt = new SqlDataAdapter(cmd);
+
+                                DataTable dt = new DataTable();
+
+                                dAdapt.Fill(dt);
+
+                                newvenomerid = long.Parse(dt.Rows[0][0].ToString());dgvVen
+
+                                if (venomerid == 0)
+                                    row.Cells["idVendor"].Value = newvenomerid;
+                            }
+
+                            catch (Exception ex)
+
+                            {
+                                csMessageBox.Show("Exception Error:" + ex.Message,
+
+                                "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                            }
+
+                        }
+                        Globals.glCloseSqlConn();
+                    }
+                    Globals.glCloseSqlConn();
+                }
+
+            }
+        }
+
+        private void dgvVen_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvVen_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            bool cancel = true;
+
+            DataGridViewRow row = this.dgvVen.CurrentRow;
+            String name = row.Cells["nameVendor"].Value.ToString().Trim();
+
+            if (row.Cells[idcolumn].Value != DBNull.Value &&
+               csMessageBox.Show("Delete the user:" + name, "Please confirm.",
+                 MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (Globals.glOpenSqlConn())
+                {
+
+                    SqlCommand cmd = new SqlCommand("dbo.spVendorsDelete", Globals.sqlconn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@vid", Convert.ToInt64(row.Cells[idcolumn].Value));
+                    cmd.ExecuteNonQuery();
+
+                    cancel = false;
+
+                }
+                Globals.glCloseSqlConn();
+            }
+            else e.Cancel = true;
+        }
+        private void EditGrid()
+        {
+            this.dgvVen.Columns["idVendor"].Visible = true;
+            this.dgvVen.Columns["nameVendor"].HeaderText = "Login Name";
+            this.dgvVen.Columns["addressVendor"].HeaderText = "Address";
+            this.dgvVen.Columns["emailVendor"].HeaderText = "Email";
+            this.dgvVen.Columns["contactVendor"].HeaderText = "Contact";
+
+            this.BackColor = Globals.gDialogBackgroundColor;
+
+            this.dgvVen.BackgroundColor = Globals.gGridOddRowColor;
+            this.dgvVen.AlternatingRowsDefaultCellStyle.BackColor = Globals.gGridEvenRowColor;
+
+            this.dgvVen.EnableHeadersVisualStyles = false;
+            this.dgvVen.ColumnHeadersDefaultCellStyle.BackColor = Globals.gGridHeaderColor;
+        }
+    }
+}
